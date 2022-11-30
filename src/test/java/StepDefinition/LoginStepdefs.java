5b4a2b01e84fd2e4068dac2e8d3ca35c7ff33a0d
muthu <marxmuthu2020@gmail.com>
2022-12-01 02:10:19 +0530

    code fix

--- src/test/java/StepDefinition/LoginStepdefs.java
+++ src/test/java/StepDefinition/LoginStepdefs.java
@@ -15,7 +15,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class LoginTest extends BaseAction {
+public class LoginStepdefs extends BaseAction {
 
 	public static BorrowCalPage borrowCalPage;
 	public static Properties envData;
